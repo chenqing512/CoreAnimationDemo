5f55c87095041c8f81d2882be88ba5f9aac19303
chenqing512 <chenqing512@sina.com>
2017-08-12 09:17:27 +0800

    Initial Commit

---
+++ CoreAnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreAnimationDemo
+//
+//  Created by ChenQing on 17/8/12.
+//  Copyright © 2017年 ChenQing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
